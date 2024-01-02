e90f6d8db8e2eb18015a7b9ed4b96ac2867164fb
isley-github <minkyeong.study@gmail.com>
2024-01-02 23:48:28 +0900

    how to use comment!

--- src/CommentJava.java
+++ src/CommentJava.java
@@ -1,7 +1,11 @@
 public class CommentJava {
     public static void main(String[] args) {
         System.out.println("hello java1"); // hello java1을 출력합니다.
-        //System.out.println("hello java2");
+        System.out.println("hello java2");
+        System.out.println("sout is println code");
+        System.out.println("psvm is main code");
+        // 2개 사용 시 주석 전환
+        // /* */ 사용 시 2개 이상 줄 주석 전환
 
         /*
         System.out.println("hello java3");
