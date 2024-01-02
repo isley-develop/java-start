7edaec713d727012bd6e3047131ae989258d914f
isley-develop <minkyeong.dev@gmail.com>
2024-01-02 23:58:27 +0900

    about java

---
+++ src/StudyJava.java
@@ -0,0 +1,6 @@
+public class StudyJava {
+    public static void main(String[] args) {
+        System.out.println("자바 표준 스펙은 java 작성 기본 설계도이다.");
+        System.out.println("실제 자바 코드는 JVM이 실행시킨다.");
+    }
+}
