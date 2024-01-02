2894891d7e2279dc1cc3335383d15c27655c5d54
isley-develop <minkyeong.dev@gmail.com>
2024-01-02 23:56:34 +0900

    how to use comment!

--- src/CommentJava.java
+++ src/CommentJava.java
@@ -4,7 +4,7 @@ public class CommentJava {
         System.out.println("hello java2");
         System.out.println("sout is println code");
         System.out.println("psvm is main code");
-        System.out.println("JVM activate .class file...");
+        System.out.println("JVM activate .class file");
         // 2개 사용 시 주석 전환
         // /* */ 사용 시 2개 이상 줄 주석 전환
 
