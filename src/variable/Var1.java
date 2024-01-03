0479ff343a0d5851fffa0d4418d54a2de9555366
isley-develop <minkyeong.dev@gmail.com>
2024-01-03 23:48:26 +0900

    about variable

---
+++ src/variable/Var1.java
@@ -0,0 +1,9 @@
+package variable; // 1번째 단계: 소속된 패키지 선언 (인텔리제이에서 자동 생성)
+
+public class Var1 {
+    public static void main(String[] args) {
+        System.out.println(10);
+        System.out.println(10);
+        System.out.println(10);
+    }
+}
