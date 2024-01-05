0759b58317393dfd4135d3accf4979b8073d5e73
isley-develop <minkyeong.dev@gmail.com>
2024-01-05 23:59:17 +0900

    Rules of Java

---
+++ src/variable/Var0.java
@@ -0,0 +1,12 @@
+package variable;
+public class Var0 {
+    public static void main(String[] args) {
+        // 변수명 표기법
+        System.out.println("Class 첫 글자는 대문자 - Var0 ");
+        System.out.println("Class 제외 나머지 첫글자는 소문자 - int a = 100; ");
+        System.out.println("띄어쓰기 X -> 낙타표기법으로 띄어쓰기 대신 대문자 표기 - userAccount (= user account)");
+        System.out.println("상수의 경우 모두 대문자 + 언더바 - USER_LIMIT ");
+        System.out.println("패키지는 전부 소문자 사용");
+        System.out.println("변수명은 용도가 잘 드러나게 지어야 한다.");
+    }
+}
