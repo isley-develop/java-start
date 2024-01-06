5493a9043922d01cbc3702f48a77346ff7a48a4f
isley-develop <minkyeong.dev@gmail.com>
2024-01-06 23:30:52 +0900

    about operator1

---
+++ src/operator/Operator1.java
@@ -0,0 +1,17 @@
+package operator;
+
+public class Operator1 {
+    public static void main(String[] args) {
+        // 변수 초기화
+        int a = 5;
+        int b = 2;
+
+        // 덧셈
+        int sum = a + b;
+        System.out.println("a + b =" + sum);
+
+        // 뺄셈
+        int diff = a - b;
+        System.out.println("a - b =" + diff);
+    }
+}
