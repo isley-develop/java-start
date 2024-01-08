437dcce01e489004eb4e918637e2270567345dc8
isley-develop <minkyeong.dev@gmail.com>
2024-01-08 23:33:29 +0900

    about operator4

--- src/operator/OperatorAdd2.java
+++ src/operator/OperatorAdd2.java
@@ -4,12 +4,13 @@ public class OperatorAdd2 {
     public static void main(String[] args) {
         // 전위 증감 연산자를 사용해보자
         int a = 1;
-        int b = 0;
+        int b;
         b = ++a;
         System.out.println("a = " + a + "b = " + b);
         b = ++a;
         System.out.println("a = " + a + "b = " + b);
         b = b + a;
         System.out.println(b);
+
     }
 }
